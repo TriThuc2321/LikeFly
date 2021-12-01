c61071a1d791e4487cb48db9631eafead591e8da
HuynhVong <71302943+HuynhVong@users.noreply.github.com>
2021-12-01 09:53:55 +0700

    User Information done
    Updated by Huỳnh

---
+++ LikeFly/LikeFly.Android/CustomEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using LikeFly;
+using LikeFly.Droid;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+
+namespace LikeFly.Droid
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+
+                var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
+                layoutParams.SetMargins(0, 0, 0, 0);
+                LayoutParameters = layoutParams;
+                Control.LayoutParameters = layoutParams;
+                Control.SetPadding(0, 0, 0, 0);
+                SetPadding(0, 0, 0, 0);
+            }
+        }
+    }
+}
