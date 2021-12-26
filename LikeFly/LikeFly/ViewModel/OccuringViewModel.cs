4272d9953a025597d35947c2f6447672ebdec418
Linh Nguyen <72326769+khanhlinh1406@users.noreply.github.com>
2021-12-26 20:48:34 +0700

    ..

--- LikeFly/LikeFly/ViewModel/OccuringViewModel.cs
+++ LikeFly/LikeFly/ViewModel/OccuringViewModel.cs
@@ -1,22 +1,140 @@
 ﻿using LikeFly.Core;
+using LikeFly.Database;
+using LikeFly.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace LikeFly.ViewModel
 {
     public class OccuringViewModel: ObservableObject
     {
-        Shell shell;
+        Shell currentShell;
         INavigation navigation;
-
+        public Command MenuCommand { get; }
         public OccuringViewModel() { }
 
         public OccuringViewModel(INavigation navigation, Shell shell)
         {
-            this.shell = shell;
+            this.currentShell = shell;
             this.navigation = navigation;
+            TicketList = new ObservableCollection<BookedTicket>();
+            MenuCommand = new Command(openMenu);
+            SetInformation();
+            SortingTicket();
+        }
+
+        public bool checkFlightStatus(Flight flight)
+        {
+            /// CancelVisible = true;
+
+            string[] flightStartDate = flight.StartDate.Split('/');
+            string[] duration = flight.Duration.Split('h');
+            string[] flightStartTime = flight.StartTime.Split(':');
+            DateTime timeStart = new DateTime(
+                int.Parse(flightStartDate[2]),
+                int.Parse(flightStartDate[1]),
+                int.Parse(flightStartDate[0]),
+                int.Parse(flightStartTime[0]),
+                int.Parse(flightStartTime[1]),
+                0
+                );
+
+            DateTime currentTime = DateTime.Now.AddDays(0);
+            TimeSpan interval = timeStart.Subtract(currentTime);
+
+            /// string maxDuration = int.Parse(duration[0]) > int.Parse(duration[1]) ? duration[0] : duration[1];
+
+            string maxDuration = duration[1] == null ?
+                (int.Parse(duration[0]) * 60 * 60).ToString() :
+                (int.Parse(duration[0]) * 60 * 60 + int.Parse(duration[1]) * 60).ToString();
+
+            // Thoi gian bat dau flight den current time
+            double count = interval.Days * 60 * 60;
+            if (count > 0)
+            {
+                
+                return false;
+            }
+            if (count <= 0 && Math.Abs(count) <= int.Parse(maxDuration))
+            {
+                return true;
+            }
+
+            return false;   
+        }
+        void SetInformation()
+        {
+            foreach (var ticket in DataManager.Ins.ListBookedTickets)
+            {
+                if (ticket.Email == DataManager.Ins.CurrentUser.email && checkFlightStatus(ticket.Flight))
+                {
+                    TicketList.Add(ticket);
+                }
+            }
+        }
+
+        private void openMenu(object obj)
+        {
+            currentShell.FlyoutIsPresented = !currentShell.FlyoutIsPresented;
+        }
+        public ICommand SelectedCommand => new Command<object>((obj) =>
+        {
+            BookedTicket result = obj as BookedTicket;
+            if (result != null)
+            {
+                DataManager.Ins.CurrentBookedTicket = result;
+                DataManager.Ins.CurrentFlight = result.Flight;
+                DataManager.Ins.CurrentInvoice = result.Invoice;
+             ///   navigation.PushAsync(new TourScheduleView());
+                SelectedTicket = null;
+            }
+        });
+
+        private ObservableCollection<BookedTicket> _ticketList;
+        public ObservableCollection<BookedTicket> TicketList
+        {
+            get { return _ticketList; }
+            set
+            {
+                _ticketList = value;
+                OnPropertyChanged("TicketList");
+            }
+        }
+
+
+        private BookedTicket selectedTicket;
+        public BookedTicket SelectedTicket
+        {
+            get { return selectedTicket; }
+            set
+            {
+                selectedTicket = value;
+                OnPropertyChanged("SelectedTour");
+
+            }
+        }
+
+        void SortingTicket()
+        {
+            // Xep giam dan
+            for (int i = 0; i < TicketList.Count - 1; i++)
+            {
+                for (int j = i + 1; j < TicketList.Count; j++)
+                {
+                    if (DateTime.Parse(TicketList[i].BookTime) < DateTime.Parse(TicketList[j].BookTime))
+                    {
+                        BookedTicket tmp = new BookedTicket();
+                        tmp = TicketList[i];
+                        TicketList[i] = TicketList[j];
+                        TicketList[j] = tmp;
+                    }
+                }
+            }
         }
     }
 }
+
